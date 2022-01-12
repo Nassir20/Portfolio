dce20d7aecbd7c480f3079775a8a69a0d2f00f77
unknown <mail.bibioune@etu.univ-st-etienne.fr>
2022-01-12 18:54:13 +0100

    First Commit

---
+++ src/Controller/NailPortfolioController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NailPortfolioController extends AbstractController
+{
+    /**
+     * @Route("/", name="/")
+     */
+    public function index(): Response
+    {
+        return $this->render('nail_portfolio/index.html.twig', [
+            'controller_name' => 'NailPortfolioController',
+        ]);
+    }
+     /**
+     * @Route("/cv", name="cv")
+     */
+    public function cv(): Response
+    {
+        return $this->render('nail_portfolio/cv.html.twig');
+    }
+     /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('nail_portfolio/contact.html.twig');
+    }
+     /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function portfolio(): Response
+    {
+        return $this->render('nail_portfolio/portfolio.html.twig');
+    }
+    /**
+     * @Route("/mesloisirs", name="mesloisirs")
+     */
+    public function mesloisirs(): Response
+    {
+        return $this->render('nail_portfolio/mesloisirs.html.twig');
+    }
+}
